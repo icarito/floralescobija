4d4574a15608baece6e4b1c4a46a3d8a40e375a0
Sebastian Silva <sebastian@fuentelibre.org>
2023-03-15 00:13:10 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './fondo.jpg';
 import './App.css';
 
 function App() {
@@ -6,17 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="App-link" href="https://www.instagram.com/florales_cobija/">Instagram</a>
+        <a className="App-link" href="https://www.facebook.com/floralescobija">Facebook</a>
+        <a className="App-link" href="https://api.whatsapp.com/send?phone=+59177100737&text=Hola%20Florales%20Cobija,%20deseo%20m%C3%A1s%20informaci%C3%B3n%20de%20sus%20ramos%20de%20flores.">Whatsapp</a>
       </header>
     </div>
   );
