c19325138f8a73a2b54ab5a07bc9422353a948a7
Sebastian Silva <sebastian@fuentelibre.org>
2023-08-13 18:16:22 -0500

    cambios incompletos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
 import { Gallery } from './component/Gallery';
+import { IonFooter, IonHeader, IonToolbar } from '@ionic/react';
+import { IonButton, IonIcon } from '@ionic/react';
 import { useState } from 'react';
+import '@ionic/react/css/core.css';
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+import { rose } from 'ionicons/icons';
+import { setupIonicReact } from '@ionic/react';
+
+setupIonicReact();
 
 function App() {
   const [showProd, setShowProd] = useState(false);
@@ -9,7 +20,13 @@ function App() {
       { showProd &&
         <Gallery></Gallery>
       }
-      <button onClick={()=>setShowProd(!showProd)}>Productos</button>
+    <IonFooter>
+      <IonToolbar>
+        <IonButton onClick={()=>setShowProd(!showProd)}>
+      <IonIcon icon={rose} size="large"></IonIcon>
+        </IonButton>
+      </IonToolbar>
+    </IonFooter>
     </div>
   );
 }
--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -1,4 +1,5 @@
 import './Gallery.css'
+import { IonContent, IonCard, IonImg, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 import data from '../mock/products.json';
 
 export function Gallery() {
@@ -6,17 +7,17 @@ export function Gallery() {
   const currency = 'Bs.'
 
   return (
-    <div className="Gallery-container">
+    <IonContent>
       { data.map((prod)=>
-        <div className="Gallery-card" key={prod.id}>
-          <img alt={ 'foto de ' + prod.name } src={ './images/' + prod.photo }></img>
+        <IonCard className="Gallery-card" key={prod.id}>
+          <IonImg className="Gallery-img" alt={ 'foto de ' + prod.name } src={ './images/' + prod.photo }/>
           <div className="Gallery-card-body">
             <label>{ prod.name }</label>
             <label>{ prod.price} { currency }</label>
           </div>
-        </div>
+        </IonCard>
       )
       }
-    </div>
+    </IonContent>
   )
 }
