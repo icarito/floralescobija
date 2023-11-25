83551c8561fe242229d902911c6f47d9eaff2efb
Sebastian Silva <sebastian@fuentelibre.org>
2023-11-25 16:16:05 -0500

    calcula total

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { getProductos } from "../lib/firebase";
 import "./gallery.css";
+import { useNavigate } from "react-router-dom";
 
 export default function FloreriaGallery({addToCart}) {
     const [productos, setProductos] = useState([]);
+    const navigateTo = useNavigate();
 
     useEffect(() => {
         const unsubscribe = getProductos(snapshot => {
@@ -15,11 +17,16 @@ export default function FloreriaGallery({addToCart}) {
         return unsubscribe
     })
 
+    function handleClick(product){
+        addToCart(product)
+        navigateTo("/formulario")
+    }
+
     return (
         <div className="Gallery">
         {
             productos.map(producto => (
-                <button onClick={()=>addToCart(producto)} style={{backgroundImage: 'url(' + producto.imagen +')'}} className="producto" key={producto.id}>
+                <button onClick={()=>handleClick(producto)} style={{backgroundImage: 'url(' + producto.imagen +')'}} className="producto" key={producto.id}>
                     <img className="imagen" src={producto.imagen} alt={producto.nombre} />
                     <h2 className="nombre">{producto.nombre}</h2>
                     <p className="descripcion">{producto.descripcion}</p>
